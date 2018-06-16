c1919d9eacdc5c574c49904b67bd70fcf841c86c
Mohammed Hatim El Abida <h.elabida@gmail.com>
2018-06-16 04:17:53 +0200

    some other enhancment...

--- test3.cpp
+++ test3.cpp
@@ -14,19 +14,21 @@ class Less_than
 
 int main()
 {
+    std::cout << std::boolalpha;
+
     Less_than<int> lt_10(10);      //ctor
     Less_than<int> lt_50{50};     //ctor
-    std::cout << "lt_10(24) : " << std::boolalpha << lt_10(24) << "\n";
-    std::cout << "lt_50(24) : " << std::boolalpha << lt_50(24) << "\n";
+
+    std::cout << "lt_10(24) : " << lt_10(24) << "\n";
+    std::cout << "lt_50(24) : " << lt_50(24) << "\n";
     std::cout << std::endl;
 
 
     Less_than<char> lt_a('a');
     Less_than<char> lt_c('c');
 
-    std::cout << "lt_a('b') : " << std::boolalpha << lt_a('b') << "\n";
-    std::cout << "lt_c('b') : " << std::boolalpha << lt_c('b') << "\n";
+    std::cout << "lt_a('b') : " << lt_a('b') << "\n";
+    std::cout << "lt_c('b') : " << lt_c('b') << "\n";
 
-    std::cout << "true : " << true << "\n";
     return 0;
 }
